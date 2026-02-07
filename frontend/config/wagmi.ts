b9e5f2e8f35dc851adf7a774abaae44651c7ad8b
Md. Faisal Tabrez <mfaisaltabrez_cse230535@mgit.ac.in>
2026-02-07 10:23:01 +0530

    feat: Refactor wagmi configuration to use getDefaultConfig for improved structure and maintainability

--- frontend/config/wagmi.ts
+++ frontend/config/wagmi.ts
@@ -1,10 +1,26 @@
 import { http, createConfig } from 'wagmi'
 import { baseSepolia } from 'wagmi/chains'
+import { getDefaultConfig } from 'connectkit'
 
-export const config = createConfig({
-  chains: [baseSepolia],
-  ssr: true,
-  transports: {
-    [baseSepolia.id]: http('https://sepolia.base.org'),
-  },
-})
+export const config = createConfig(
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [baseSepolia],
+    transports: {
+      // RPC URL for each chain
+      [baseSepolia.id]: http('https://sepolia.base.org'),
+    },
+
+    // Required API Keys
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "YOUR_PROJECT_ID",
+
+    // Required App Info
+    appName: "CyberDAO",
+
+    // Optional App Info
+    appDescription: "Cyber Security DAO",
+    appUrl: "https://cyberdao.demo", 
+    appIcon: "https://family.co/logo.png", 
+    ssr: true, // If using Next.js
+  }),
+)
